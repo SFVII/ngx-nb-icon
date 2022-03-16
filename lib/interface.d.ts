22218cc4fc49d5790449fc330a75c6bedc93cd26
Brice Daupiard <brice.daupiard@nowbrains.com>
2022-03-16 19:31:36 +0100

    fix: finally

---
+++ lib/interface.d.ts
@@ -0,0 +1,11 @@
+/***********************************************************
+ **  @project
+ **  @file
+ **  @author Brice Daupiard <brice.daupiard@nowbrains.com>
+ **  @Date 16/03/2022
+ **  @Description
+ ***********************************************************/
+export interface INowboardIconConfig {
+    primary: string;
+    disabled_color: string;
+}
